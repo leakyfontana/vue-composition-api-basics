2af4d150ab055fa5f199293072f7501b9f14df39
Xander Dyer <xdyer@confirmedapp.com>
2023-05-30 16:04:30 -0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="home">
+
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}:</h3>
+  
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
+
+  </div>
+</template>
+
+<!-- Composition API implementation (script setup pattern) -->
+<script setup>
+  import { reactive } from 'vue';
+
+  const appTitle = 'My Amazing Counter App';
+
+  // const counter = ref(0);
+  // const counterTitle = ref('My Counter');
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter',
+  });
+
+  const increaseCounter = () => {
+    counterData.count++;
+  }
+
+  const decreaseCounter = () => {
+    counterData.count--;
+  }
+</script>
+
+<!-- Composition API implementation (function setup pattern) -->
+<!-- 
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0)
+
+    const increaseCounter = () => {
+      counter.value++
+    }
+
+    const decreaseCounter = () => {
+      counter.value--
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter,
+    }
+  }
+}
+</script> 
+-->
+
+<!-- Options API implementation -->
+<!-- 
+  <script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    },
+  }
+}
+</script> 
+-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+</style>
